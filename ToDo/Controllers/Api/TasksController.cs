7ef2619a530cee6b0346ff7db630e3f599b77ab8
ayshasardar <aysha.kcompute@gmail.com>
2026-04-30 23:59:28 +0500

    I created this API endpoint to expose my ML prediction as a reusable service outside the MVC flow, not just during the 'ADD Task' form submission. It allows features like AJAX live prediction, mobile apps, or future frontends to call it independently.

--- ToDo/Controllers/Api/TasksController.cs
+++ ToDo/Controllers/Api/TasksController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ToDoDemo.Models;
 using ToDoDemo.Services.Implementations;
 using ToDoDemo.Services.Interfaces;
 
@@ -22,5 +23,19 @@ public class TasksController : ControllerBase
             return NotFound();
 
         return Ok(new { success = true });
+    }
+
+    [HttpPost("predict-priority")]
+    //I created this API endpoint to expose my ML prediction as a reusable service outside the
+    //MVCflow, not just during the 'ADD Task' form submission 
+    //It allows features like AJAX live prediction, mobile apps, or future frontends to call it independently.
+    public IActionResult PredictPriority([FromBody] ToDo task)
+    {
+        if (task == null)
+            return BadRequest();
+
+        var result = _toDoService.PredictPriority(task);
+
+        return Ok(new { predictedPriority = result });
     }
 }
